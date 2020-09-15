026b659b433d0384fa5015e3a73eeba26fde8b4e
gustavo <gusfelipes@gmail.com>
2020-09-15 15:25:28 -0300

    add search page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import api from "./services/api.js";
+
+import "./global.css";
+import "./App.css";
+import "./Header.css";
+import "./Main.css";
+
+import RepoItem from "./components/RepoItem/index.js";
+import { Link } from "react-router-dom";
 
 function App() {
+  const [repos, setRepos] = useState([]);
+  const [user, setUser] = useState([]);
+
+  async function handleSearchRepos(e) {
+    e.preventDefault();
+    console.log(user);
+    const response = await api.get(`/repos/${user}`);
+    console.log(response);
+    setRepos(response.data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="app">
+      <header>
+        <form onSubmit={handleSearchRepos}>
+          <input
+            type="text"
+            name="user"
+            id="user"
+            required
+            placeholder="Digite o nome do usuário"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+          <button type="submit">Pesquisar</button>
+        </form>
       </header>
+
+      <main>
+        <div className="repos-list">
+          <ul>
+            {repos.map((repo) => (
+              <Link to="/">
+                <RepoItem key={repo.id} repo={repo} />
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/components/RepoItem/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+function RepoItem({ repo }) {
+  console.log(repo);
+  return (
+    <li className="repo-item">
+      <p>
+        Nome:
+        <strong>{repo.name}</strong>
+      </p>
+      <p>Autor: {repo.owner.login}</p>
+      <p>Descrição: {repo.description}</p>
+      <p>
+        Estrelas: <strong>{repo.stargazers_count}</strong>
+      </p>
+    </li>
+  );
+}
+
+export default RepoItem;
